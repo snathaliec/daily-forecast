8a0fa7c8351d90bd1c19648d8b1425bf70d7275a
NathalieSantana <snathaliec@gmail.com>
2023-04-11 23:17:35 -0400

    Add phrases arrays

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log("Is it working?");
+const moods = ["sunny", "cloudy", "windy", "rainy", "stormy"];
+const thoughts = [
+    "You have to dream before your dreams can come true. – A.P.J Abdul Kalam", 
+    "The best way out is always through. – Robert Frost", "Rational thoughts never drive people’s creativity the way emotions do. – Neil deGrasse Tyson", 
+    "It is never too late to be what you might have been. – George Eliot", 
+    "Positive thinking will let you do everything better than negative thinking will. – Zig Ziglar",
+    "Everyone wants happiness, no one wants pain, but you can’t make a rainbow without a little rain. –  Zion Lee"
+];
+const accessories = ["Jacket", "Smile", "Sunglasses", "Shoes", "Water", "Snacks", "Keys"];
